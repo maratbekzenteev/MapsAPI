66bd468446e7b662a040010bcd98a26ac5a81f02
marat <maratbekzenteev@yandex.ru>
2022-02-13 17:12:34 +0300

    Переделано задание области показа, добавлена возможность изменения масштаба

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ MAP_NAME = 'map.png'
 
 long = 37.640280  # longitude, долгота
 lat = 55.765665  # latitude, широта
-scale = 15  # параметр z, масштаб
-mode = 'map' # режим отображения
+scale = 1  # масштаб, по формуле из него выводится область показа
+SCALE_TO_SPN = "str(10 ** (scale - 5))"
+mode = "map"  # режим отображения
 
 pygame.init()
-size = 650, 450
+size = 450, 450
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Карта")
 
@@ -28,7 +29,7 @@ static_api = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(static_api, params={
     'll': str(long) + ',' + str(lat),
     'size': str(size[0]) + ',' + str(size[1]),
-    'z': scale,
+    'spn': eval(SCALE_TO_SPN) + ',' + eval(SCALE_TO_SPN),
     'l': mode
 })
 
@@ -38,9 +39,36 @@ screen.blit(load_image(MAP_NAME), (0, 0))
 pygame.display.flip()
 
 running = True
+new_frame = False  # флаг, показывающий необходимость повторной отрисовки
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == 1073741899:
+                scale -= 1
+                new_frame = True
+            elif event.key == 1073741902:
+                scale += 1
+                new_frame = True
+            if scale < 1:
+                scale = 1
+            elif scale > 6:
+                scale = 6
+    keys = pygame.key.get_pressed()
 
-    # keys = pygame.key.get_pressed()
+    # повторная отрисовка кадра
+    if new_frame:
+        response = requests.get(static_api, params={
+            'll': str(long) + ',' + str(lat),
+            'size': str(size[0]) + ',' + str(size[1]),
+            'spn': eval(SCALE_TO_SPN) + ',' + eval(SCALE_TO_SPN),
+            'l': mode
+        })
+
+        with open(MAP_NAME, "wb") as file:
+            file.write(response.content)
+        screen.fill((0, 0, 0))
+        screen.blit(load_image(MAP_NAME), (0, 0))
+        pygame.display.flip()
+        new_frame = False
